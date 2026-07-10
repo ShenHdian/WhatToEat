2a8716d360157a23c89571303912e9bf6a3e9a33
ShenHdou <15574380273@163.com>
2026-07-11 00:20:31 +0800

    fix: deploy.sh 加载 nvm 环境变量，修复 cron 下找不到 npm/pm2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export PATH="$NVM_DIR/versions/node/v24.18.0/bin:$PATH"
+
 cd /etc/whattoeat
 
 git fetch origin
@@ -14,18 +18,14 @@ echo "[$(date)] New updates found! Deploying..."
 
 git pull origin master
 
-# Install deps
 cd /etc/whattoeat/server && npm install
 cd /etc/whattoeat/client && npm install
 
-# Build frontend
 cd /etc/whattoeat/client && npm run build
 
-# Kill old process on port 3001 (if any)
 fuser -k 3001/tcp 2>/dev/null
 sleep 1
 
-# Start fresh with PM2
 cd /etc/whattoeat/server
 pm2 start server.js --name whattoeat --update-env
 pm2 save
